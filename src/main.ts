66d2138fb9372c6f25d9be0f3249d9b1f8a2c4a5
Amin NAIRI <nairi.amin@gmail.com>
2020-11-28 12:04:05 +0100

    :sparkles: added a middleware function

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,47 @@
 interface Property {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 const random = <T>(values: T[]): T => {
-    const items = values;
+  const items = values;
 
-    items.sort(() => Math.random() - 0.5);
+  items.sort(() => Math.random() - 0.5);
 
-    return items[0];
+  return items[0];
 };
 
-const createObjectGenerator = (properties: Property) => (): Property => {
-    const record: Property = {};
+const createObjectGenerator = (properties: Property) => (middleware: (value: Property) => Property | null = null): Property => {
+  const record: Property = {};
 
-    for (const [key, values] of Object.entries(properties)) {
-        if ("function" === typeof values) {
-            /* eslint-ignore-next-line */
-            record[key] = values as Function();
+  for (const [key, values] of Object.entries(properties)) {
+    if ("function" === typeof values) {
+      /* eslint-ignore-next-line */
+      record[key] = values as Function();
 
-            continue;
-        }
-
-        record[key] = random(values);
+      continue;
     }
 
-    return record;
+    record[key] = random(values);
+  }
+
+  if ("function" === typeof middleware) {
+    return middleware(record);
+  }
+
+  return record;
 };
 
-const createArrayGenerator = <T>(values: T[]) => (): T => random(values);
+const createArrayGenerator = <T>(values: T[]) => {
+  return (middleware: (value: T) => T | null = null): T => {
+    if ("function" === typeof middleware) {
+      return middleware(random(values));
+    }
+
+    return random(values);
+  };
+}
 
 export {
-    createObjectGenerator,
-    createArrayGenerator
+  createObjectGenerator,
+  createArrayGenerator
 };
