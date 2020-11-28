652878cdeba903e89f81b5e4f2eaada4ef1b72f6
Amin NAIRI <nairi.amin@gmail.com>
2020-11-28 16:58:46 +0100

    :package: new production build

--- lib/main.d.ts
+++
@@ -1,6 +0,0 @@
-interface Property {
-    [key: string]: any;
-}
-declare const createObjectGenerator: (properties: Property) => () => Property;
-declare const createArrayGenerator: <T>(values: T[]) => () => T;
-export { createObjectGenerator, createArrayGenerator, };
