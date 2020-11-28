b2b896966429451d9125db4d1fc96942477acc32
Amin NAIRI <nairi.amin@gmail.com>
2020-11-28 16:58:25 +0100

    :sparkles: added middleware unit tests

--- tests/create-array-generator.spec.ts
+++ tests/create-array-generator.spec.ts
@@ -1,9 +1,26 @@
-import {createArrayGenerator} from "../src/main";
+import {createGenerator} from "../src/index";
 
-describe("createArrayGenerator", () => {
-    it("should return one value", () => {
-        const generateValue = createArrayGenerator([1, 2, 3]);
+describe("createGenerator", () => {
+  it("should return one value", () => {
+    const FIRST_VALUE = 1;
+    const SECOND_VALUE = 1;
+    const THIRD_VALUE = 1;
 
-        expect(typeof generateValue()).toStrictEqual("number");
-    });
+    const generateValue = createGenerator([FIRST_VALUE, SECOND_VALUE, THIRD_VALUE]);
+
+    expect(typeof generateValue()).toStrictEqual("number");
+  });
+
+  it("should return one value with middleware applied", () => {
+    const FIRST_VALUE = 2;
+    const SECOND_VALUE = 2;
+    const THIRD_VALUE = 2;
+    const EXPECTED_VALUE = 4;
+
+    const generateValue = createGenerator([FIRST_VALUE, SECOND_VALUE, THIRD_VALUE]);
+    const value = generateValue(oldValue => oldValue * oldValue);
+
+    expect(typeof value).toStrictEqual("number");
+    expect(value).toStrictEqual(EXPECTED_VALUE);
+  });
 });
--- tests/create-object-generator.spec.ts
+++ tests/create-object-generator.spec.ts
@@ -1,18 +1,43 @@
-import {createObjectGenerator} from "../src/main";
+import {createObjectGenerator} from "../src/index";
 
 describe("createObjectGenerator", () => {
-    it("should return an object", () => {
-        const generateUser = createObjectGenerator({
-            firstname: ["John", "Foo", "Bar"],
-            lastname: ["Doe", "Baz", "Faz"],
-            age: [27, 28, 29]
-        });
+  it("should return an object", () => {
+    const FIRST_AGE = 27;
+    const SECOND_AGE = 27;
+    const THIRD_AGE = 27;
 
-        const user = generateUser();
-
-        expect(typeof user).toStrictEqual("object");
-        expect(typeof user.firstname).toStrictEqual("string");
-        expect(typeof user.lastname).toStrictEqual("string");
-        expect(typeof user.age).toStrictEqual("number");
+    const generateUser = createObjectGenerator({
+      age: [FIRST_AGE, SECOND_AGE, THIRD_AGE],
+      firstname: ["John", "Foo", "Bar"],
+      lastname: ["Doe", "Baz", "Faz"]
     });
+
+    const user = generateUser();
+
+    expect(typeof user).toStrictEqual("object");
+    expect(typeof user.firstname).toStrictEqual("string");
+    expect(typeof user.lastname).toStrictEqual("string");
+    expect(typeof user.age).toStrictEqual("number");
+  });
+
+  it("should return an object with middlewares applied", () => {
+    const FIRST_AGE = 2;
+    const SECOND_AGE = 2;
+    const THIRD_AGE = 2;
+    const EXPECTED_AGE = 4;
+
+    const generateUser = createObjectGenerator({
+      age: [FIRST_AGE, SECOND_AGE, THIRD_AGE],
+      firstname: ["John", "Foo", "Bar"],
+      lastname: ["Doe", "Baz", "Faz"]
+    });
+
+    const user = generateUser({age: oldAge => oldAge * oldAge});
+
+    expect(typeof user).toStrictEqual("object");
+    expect(typeof user.firstname).toStrictEqual("string");
+    expect(typeof user.lastname).toStrictEqual("string");
+    expect(typeof user.age).toStrictEqual("number");
+    expect(user.age).toStrictEqual(EXPECTED_AGE);
+  });
 });
